25853de43a48194b4527e949308b13f4b0047379
manish-infolanze <manish.infolanze@gmail.com>
2025-03-04 17:10:00 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import React from 'react';
 import Admin from "./components/Admin/admin";
 import User from './components/User/user';
 import "./App.css";
 
+function Home() {
+  const navigate = useNavigate();  // useNavigate hook for navigation
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <div>
+        <button onClick={() => navigate("/user")}>User</button>
+        <button onClick={() => navigate("/admin")}>Admin</button>
+      </div>
+    </>
+  );
+}
+
 function App() {
-  return (<>
-  <h1>Hello</h1>
+  return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/user" element={<User />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
-    </>
   );
 }
 
